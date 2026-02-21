cf51941597d460321cb783938699aa75d2a871ca
Dima Holubenko <amid310596@gmail.com>
2026-02-21 20:03:46 +0200

    task

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
-def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+def format_linter_error(error):
+    return {
+        "line": error.get("line"),
+        "column": error.get("column"),
+        "code": error.get("code"),
+        "message": error.get("message")
+    }
 
 
-def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+def format_single_linter_file(file_path, errors):
+    return {
+        "path": file_path,
+        "status": "failed" if errors else "passed",
+        "errors": [format_linter_error(e) for e in errors]
+    }
 
 
-def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+def format_linter_report(report):
+    return [format_single_linter_file(file_path, errors)
+            for file_path, errors in report.items()]
